76d9f4009e7109dbcc985d6aa787401d5466d128
lhafkamp <Luuk@120.164.028.145.hva.nl>
2017-04-13 14:47:34 +0200

    Color listens to the weather API
    Using the wunderground API for the weather to send to the nodeMCU

--- server.js
+++ server.js
@@ -1,27 +1,51 @@
 const path = require('path');
 const express = require('express');
 const request = require('request');
+
+require('dotenv').config();
 const app = express();
 
 const users = ['71B6', 'E568', 'C804'];
-const color = ['FFFFFF', '9600ff', '00ff2b'];
-var randomValue = color[Math.floor(color.length * Math.random())];
+const randomUser = users[Math.floor(users.length * Math.random())];
+const colors = ['FFFFFF', '9600ff', '00ff2b'];
+const randomColor = colors[Math.floor(colors.length * Math.random())];
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+let temp = '';
+let newColor = '';
+
+const APIKEY = process.env.API_KEY;
+
 app.get('/', function(req, res) {
+  request(`https://api.wunderground.com/api/${APIKEY}/conditions/q/autoip.json`, (error, response, body) => {
+    const data = JSON.parse(body);
+    temp = data.current_observation.temp_c;
+  });
+  if (temp > 10) {
+    newColor = 'ffa500';
+  } else {
+    newColor = '1fe3ff';
+  }
+  res.render('index');
+
+  sendColor();
+});
+
+function sendColor() {
   request({
     uri: `http://oege.ie.hva.nl/~palr001/icu/api.php`,
     qs: {
+      t: 'rdc',
       t: 'sdc',
       d: '71B6',
       td: '71B6',
-      c: randomValue
+      m: 'test',
+      c: newColor
     }
-  })
-  res.render('index');
-});
+  });
+}
 
 const port = process.env.PORT || 2500;
 

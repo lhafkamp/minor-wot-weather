485b57851855e79b47d4deb8646153eb9be2c52e
lhafkamp <Luuk@120.164.028.145.hva.nl>
2017-04-13 11:26:30 +0200

    small changes

--- server.js
+++ server.js
@@ -1,36 +1,21 @@
-const path = require('path')
-const express = require('express')
-const app = express()
+const path = require('path');
+const express = require('express');
+const request = require('request');
+const app = express();
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
-app.get('/', function(req, res) {
-  res.render('index', {
-    scores
-  })
-})
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  const name = req.query.name
-  const seconds = req.query.time / 1000
-  buttonPresses++
+  request(`http://api.giphy.com/v1/gifs/search?q=winner&api_key=dc6zaTOxFJmzC`, (error, response, body) => {
+  const data = JSON.parse(body);
   res.render('index', {
-    scores
+    data,
   });
+});
 
-  console.log(name, seconds);
-
-  scores.push({
-    name,
-    seconds
-  });
-
-  console.log(scores);
-})
-
-var port = process.env.PORT || 2300
+var port = process.env.PORT || 2500;
 
 app.listen(port, function() {
-    console.log('App is running on http://localhost:' + port)
-})
+    console.log('App is running on http://localhost:' + port);
+});

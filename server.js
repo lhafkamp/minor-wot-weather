f1a494c9bae3aed31f419769db2922f0e85f4e41
lhafkamp <Luuk@120.164.028.145.hva.nl>
2017-04-13 12:35:55 +0200

    sending colors works

--- server.js
+++ server.js
@@ -3,18 +3,27 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
+const users = ['71B6', 'E568', 'C804'];
+const color = ['FFFFFF', '9600ff', '00ff2b'];
+var randomValue = color[Math.floor(color.length * Math.random())];
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  request(`http://api.giphy.com/v1/gifs/search?q=winner&api_key=dc6zaTOxFJmzC`, (error, response, body) => {
-  const data = JSON.parse(body);
-  res.render('index', {
-    data,
-  });
+  request({
+    uri: `http://oege.ie.hva.nl/~palr001/icu/api.php`,
+    qs: {
+      t: 'sdc',
+      d: '71B6',
+      td: '71B6',
+      c: randomValue
+    }
+  })
+  res.render('index');
 });
 
-var port = process.env.PORT || 2500;
+const port = process.env.PORT || 2500;
 
 app.listen(port, function() {
     console.log('App is running on http://localhost:' + port);

935f0b26e39a045b005b327e045b9d5d9bc3f2a0
Sjoerdbeentjes <mail@sjoerdbeentjes.nl>
2017-04-18 12:04:57 +0200

    Code optimization (#1)
    * sendColor function for each user in 'users' array
    
    * removed sendColor outside users.forEach, because that wouldn't work
    
    * moved stuff that sets color inside async request callback

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const request = require('request');
 const users = ['71B6', 'E568', 'C804'];
 
 // empty variables for later use
-let temp = '';
-let newColor = '';
+let temp;
+let newColor;
 
 // env key
 require('dotenv').config();
@@ -21,14 +21,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // function for sending data to the buttons
-function sendColor() {
+function sendColor(user) {
   request({
     uri: `http://oege.ie.hva.nl/~palr001/icu/api.php`,
     qs: {
       t: 'rdc',
       t: 'sdc',
-      d: '71B6',
-      td: '71B6',
+      d: 'E568',
+      td: user,
       c: newColor
     }
   });
@@ -44,17 +44,14 @@ app.get('/', function(req, res) {
     res.render('index', {
       temp: temp
     });
+
+    // if the celcius is higher than 18 store orange, else store blue
+    temp > 18 ? newColor = 'ffa500' : newColor = '1fe3ff';
+
+    users.forEach(user => {
+      sendColor(user);
+    });
   });
-
-  // if the celcius is higher than 18 store orange, else store blue
-  if (temp > 18) {
-    newColor = 'ffa500';
-  } else {
-    newColor = '1fe3ff';
-  }
-
-  // run the sendColor function
-  sendColor();
 });
 
 // run on 2500

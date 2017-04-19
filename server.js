7e5a019c84122ce50ede0459f095274b20fadffd
Merlijn Vos <merlijn.vos043@gmail.com>
2017-04-19 14:29:54 +0200

    :sparkles: Socket.io user handleling

--- server.js
+++ server.js
@@ -1,21 +1,71 @@
 const path = require('path')
+const Server = require('http').Server
 const express = require('express')
+const socketio = require('socket.io')
 const request = require('request')
 require('dotenv').config()
 
 const app = express()
+const server = Server(app)
+const io = socketio(server)
 const port = process.env.PORT || 2500
 const APIKEY = process.env.API_KEY
-
-// NodeMCU ID's from Luuk/Sjoerd/Merlijn
-// const users = ['71B6', 'E568', 'C804']
 const users = []
+const connections = []
 
 let temp
 let newColor
 
+app.use(express.static(path.join(__dirname, '/public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+// Request weather data from API
+app.get('/', (req, res) => {
+  fetchColor()
+  res.render('index', {
+    temp
+  })
+})
+
+app.get('/status', (req, res, next) => {
+  const position = users.map(x => {
+    return x.id
+  }).indexOf(req.query.id)
+  const found = users[position]
+
+  if (!found) {
+    users.push(req.query)
+    console.log(users)
+    io.sockets.emit('users', {users})
+  } else if (found) {
+    users[position].status = req.query.status
+    console.log(users)
+    io.sockets.emit('users', {users})
+  }
+
+  next()
+})
+
+app.get('*', (req, res) => {
+  res.redirect('/')
+})
+
+io.on('connection', socket => {
+  connections.push(socket)
+  console.log('Connected: %s sockets connected', connections.length)
+
+  socket.on('disconnect', () => {
+    // users.splice(users.indexOf(socket.username), 1)
+
+    connections.splice(connections.indexOf(socket), 1)
+    console.log('Disconnected: %s sockets connected', connections.length)
+  })
+})
+
+server.listen(port, () => {
+  console.log('App is running on http://localhost:' + port)
+})
 
 // Function for sending data to the buttons
 function setColor(user) {
@@ -30,15 +80,10 @@ function setColor(user) {
   })
 }
 
-function fetchColor(res) {
+function fetchColor() {
   request(`https://api.wunderground.com/api/${APIKEY}/conditions/q/autoip.json`, (error, response, body) => {
     const data = JSON.parse(body)
     temp = data.current_observation.temp_c
-
-     // Render index on '/'
-    res.render('index', {
-      temp
-    })
 
     // If the celcius is higher than 18 store orange, else store blue
     temp > 0 ? newColor = 'ffa500' : newColor = '1fe3ff'
@@ -48,29 +93,3 @@ function fetchColor(res) {
     })
   })
 }
-
-// Request weather data from API
-app.get('/', (req, res) => {
-  fetchColor(res)
-})
-
-app.get('/status', (req, res) => {
-  const position = users.map(x => {
-    return x.id
-  }).indexOf(req.query.id)
-  const found = users[position]
-
-  if (!found) {
-    users.push(req.query)
-  } else if (found) {
-    users[position].status = req.query.status
-  }
-
-  console.log(users)
-
-  res.send('status')
-})
-
-app.listen(port, () => {
-  console.log('App is running on http://localhost:' + port)
-})

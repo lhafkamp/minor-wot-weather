594c5e70ea5abd4220cf758c44144d3a5a3e13e4
Merlijn Vos <merlijn.vos043@gmail.com>
2017-04-24 16:00:02 +0200

    :bug: Fix double header send

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ let newColor
 let intermezzo = false
 let now = new Date()
 let intermezzoNoon = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 0, 0, 0) - now
-let intermezzo2PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 0, 0, 0) - now
+let intermezzo2PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 23, 0, 0) - now
 let intermezzo4PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 16, 0, 0, 0) - now
 
 if (intermezzoNoon <= 0) {
@@ -67,15 +67,16 @@ app.get('/status', (req, res, next) => {
   next()
 })
 
-app.get('/users', (req, res, next) => {
-  if (intermezzo === true) {
+app.get('/users', (req, res) => {
+  // if (intermezzo === true) {
     const activeUsers = users.filter(user => user.status === 1)
-    res.send(activeUsers.length)
+    res.send(activeUsers.length.toString())
     intermezzo = false
-  } else {
-    res.send('-1')
-  }
-  next()
+  // } else {
+    // The server successfully processed the request
+    // and is not returning any content.
+    // res.send(204)
+  // }
 })
 
 app.get('*', (req, res) => {

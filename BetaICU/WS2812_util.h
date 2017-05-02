ef4546b9d0f513d60813bab553c827ebe1addc62
lhafkamp <luukhafkamp@gmail.com>
2017-05-02 14:58:25 +0200

    lights now vanish per offline user

--- BetaICU/WS2812_util.h
+++ BetaICU/WS2812_util.h
@@ -1,5 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ400);
+Adafruit_NeoPixel userStrip = Adafruit_NeoPixel(8, LED_PIN_USER, NEO_GRB + NEO_KHZ800);
 
 void colorWipe(uint32_t c) 
 {
@@ -25,6 +26,17 @@ void setAllPixels(uint8_t r, uint8_t g, uint8_t b, float multiplier = 1.0)
   strip.show();
 }
 
+
+void setAllUserPixels(uint8_t r, uint8_t g, uint8_t b, float multiplier = 1.0) 
+{
+  for (int iPixel = 0; iPixel < LED_COUNT; iPixel++)
+    userStrip.setPixelColor(iPixel,
+                        (byte)((float)r * multiplier),
+                        (byte)((float)g * multiplier),
+                        (byte)((float)b * multiplier));
+  strip.show();
+}
+
 //This method grabs the current RGB values and current brightness and fades the colors to black
 void fadeBrightness(uint8_t r, uint8_t g, uint8_t b, float currentBrightness)
 {
@@ -40,9 +52,12 @@ void fadeBrightness(uint8_t r, uint8_t g, uint8_t b, float currentBrightness)
 // new strip for counting participants
 //////////////////////////////////////
 
-Adafruit_NeoPixel userStrip = Adafruit_NeoPixel(8, LED_PIN_USER, NEO_GRB + NEO_KHZ800);
-
 void setUserLeds(short r, short g, short b, int users) {
+
+    Serial.println(users);
+
+    setAllUserPixels(0, 0, 0, 0);
+  
     for(uint16_t i = 0; i <= users; i++) {
       userStrip.setPixelColor(i, r, g, b);
       userStrip.show();
